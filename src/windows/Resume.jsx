ec93dac77d411fe57740ee04d88f00368614420d
Arvindkr123 <thakurarvindkr10@gmail.com>
2026-02-03 23:15:08 +0530

    solve added unpagke pdf cdn

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -4,10 +4,11 @@ import { Download } from 'lucide-react'
 import React from 'react'
 
 import { Document, Page, pdfjs } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//     'pdfjs-dist/build/pdf.worker.min.mjs',
+//     import.meta.url,
+// ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
 const Resume = () => {
     return (
